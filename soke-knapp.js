8ffea75733e3bc485112d6d16a8d0dfce8f9b107
vanmelon <hms2006@online.no>
2023-10-30 09:54:17 +0100

    fik en feilkode om søkefungsjonene. måtte loade alt før javascipt konden fungerte

--- soke-knapp.js
+++ soke-knapp.js
@@ -1,24 +1,28 @@
-// Få referanser til søkeknapp og inputfelt
-var searchButton = document.getElementById("searchButton");
-var searchInput = document.getElementById("searchInput");
 
-// Legg til en klikk-hendelse for søkeknappen
-searchButton.addEventListener("click", function() {
-    var searchTerm = searchInput.value.toLowerCase(); // Konverter til små bokstaver for å unngå case-sensitivitet
+document.addEventListener("DOMContentLoaded", function() {
+    // Hent referanser til søkeknappen og inputfeltet
+    var searchButton = document.getElementById("searchButtonID");
+    var searchInput = document.getElementById("searchInputID");
 
-    // Sjekk hvilken blomst som tilsvarer søket
-    switch (searchTerm) {
-        case "jennifer-coffin-grey":
-            window.location.href = "#jennifer-coffin-grey";
-            break;
-        case "tulip":
-            window.location.href = "#tulip";
-            break;
-        case "ost":
-            window.location.href = "#ost";
-            break;
-        // Legg til flere saker for andre blomster
-        default:
-            alert("Ingen samsvar funnet.");
-    }
+    // Legg til en klikk-hendelse for søkeknappen
+    searchButton.addEventListener("click", function() {
+        // Få verdien fra inputfeltet og konverter til små bokstaver for å unngå case-sensitivitet
+        var searchTerm = searchInput.value.toLowerCase();
+
+        // Sjekk hvilken blomst som tilsvarer søket
+        switch (searchTerm) {
+            case "jennifer-coffin-grey":
+                window.location.href = "#jennifer-coffin-grey";
+                break;
+            case "tulip":
+                window.location.href = "#tulip";
+                break;
+            case "ost":
+                window.location.href = "#ost";
+                break;
+            // Legg til flere saker for andre blomster
+            default:
+                alert("Ingen samsvar funnet.");
+        }
+    });
 });
