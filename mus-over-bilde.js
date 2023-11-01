5fe99d1e015687f450a11984f46ea5ba0e54e8c5
vanmelon <hms2006@online.no>
2023-11-01 13:42:37 +0100

    text over bildet

--- mus-over-bilde.js
+++ mus-over-bilde.js
@@ -7,11 +7,12 @@ function bigImg(x) {
     x.style.height = "100%";
     x.style.width = "100%";
   }
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+  function textOverBildet() {
+    var x = document.getElementById("textOverBildetID");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+    console.log("fungerer")
   }
-}
