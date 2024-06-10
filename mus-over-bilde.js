6e4e933ef1d7dd7e52b34134abd39764762a7f0a
vanmelon <hms2006@online.no>
2024-06-10 09:53:17 +0200

    retter opp feil

--- mus-over-bilde.js
+++ mus-over-bilde.js
@@ -7,16 +7,3 @@ function bigImg(x) {
     x.style.height = "100%";
     x.style.width = "100%";
   }
-
-/* dette var en fungsjon når musen din sto over bildet spå var det tekst, men når musen din ikke var over så var det ingen tekst
- jeg ville at dette skule sje med alle, så jeg byta det fra id til class å dette ødela alt jeg vet ikke åsen jeg fikser det
- 
-  function textOverBildet() {
-    var x = document.getElementById("textOverBildetCLASS");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-  */
